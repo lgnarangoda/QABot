c7fb8c1c38957cf8e46083c844b916102a221a7c
Lasitha <narangodalasitha7@gmail.com>
2018-03-22 11:49:28 +0530

    change requirement files

--- AnswerExtract/NER.py
+++ AnswerExtract/NER.py
@@ -4,29 +4,34 @@ text = "How many papers in the queue child"
 import nltk
 import os
 def corenlp1(text):
-    # corenlp = StanfordCoreNLP('http://corenlp.run:80')
-    # core = corenlp.annotate(str(text),
-    #                    properties={
-    #                        'annotators': "parse,ner",
-    #                        'outputFormat': 'json',
-    #                        'timeout': 100000,
-    #                    })
-    # coreDict = {}
-    # print(core)
-    # l = 0
-    # for s in core['sentences']:
-    #     for t in s['tokens']:
-    #         originel_text = t['originalText']
-    #         ner = t['ner']
-    #         coreDict[originel_text.lower()] = ner
-    cur_path = os.path.dirname(__file__)
-    from nltk.tag.stanford import StanfordNERTagger
-    st = StanfordNERTagger(cur_path + "/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz",
-                           cur_path + "/stanford-ner/stanford-ner.jar")
+    corenlp = StanfordCoreNLP('http://corenlp.run:80')
+    core = corenlp.annotate(str(text),
+                       properties={
+                           'annotators': "parse,ner",
+                           'outputFormat': 'json',
+                           'timeout': 100000,
+                       })
+    coreDict = {}
+    print(core)
+    l = 0
+    for s in core['sentences']:
+        for t in s['tokens']:
+            originel_text = t['originalText']
+            ner = t['ner']
+            coreDict[originel_text.lower()] = ner
 
-    print (st.tag((text)))
+    print('test',coreDict)
+    # cur_path = os.path.dirname(__file__)
+    # java_path = "C:\Program Files\Java\jdk1.8.0_45/bin/java.exe"
+    # os.environ['JAVAHOME'] = java_path
+    #
+    # from nltk.tag.stanford import CoreNLPNERTagger
+    # # st = StanfordNERTagger(cur_path + "/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz",
+    # #                        cur_path + "/stanford-ner/stanford-ner.jar")
+    # st = CoreNLPNERTagger()
+    # print (st.tag((text)))
 # def corenlp1(text):
-# java_path = "C:\Program Files\Java\jdk1.8.0_45/bin"
+# java_path = "/usr/lib/jvm/java-6-openjdk/Java\jdk1.8.0_45/bin"
 # os.environ['JAVAHOME'] = java_path
 
 
